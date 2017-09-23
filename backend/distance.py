e1d4e9555af6eb3cd5eb3b216e40dad1ba06083c
Dan Schwartz <deschwartz13@gmail.com>
2017-09-23 20:05:00 +0800

    Closest node of list found

--- backend/distance.py
+++ backend/distance.py
@@ -2,17 +2,21 @@ from geopy.distance import vincenty
 import geocoder
 import geohash
 
-def hash_to_geo(locname):
-	for location in r.zrange(locname, 0, -1):
-	     geohash = r.geohash(locname, location)[0]
-         nodes = Geohash.decode(geohash.rstrip('0'))
-    return nodes
+# def hash_to_geo(locname):
+# 	for location in r.zrange(locname, 0, -1):
+# 	     geohash = r.geohash(locname, location)[0]
+#          nodes = Geohash.decode(geohash.rstrip('0'))
+#     return nodes
+
+def hash_to_geo(locname, city):
+	geohash = r.geohash(city, locname)
+	return Geohash.decode(geohash.rstrip('0'))
 
 def closest_node(latlng, city):
 	closest_locname = None
 	distance = None
 	for locname in r.zrange(city, 0, -1):
-		geotag = hash_to_geo(locname)
+		geotag = hash_to_geo(locname, city)
 		if distance is None:
 			distance = vincenty(latlng, geotag).km
 
