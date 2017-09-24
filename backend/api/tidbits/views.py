7fae5dd566786198e6fe9c1cd6096e6802972d42
Dan Schwartz <deschwartz13@gmail.com>
2017-09-24 13:31:16 +0800

    Adds tidbits/random

--- backend/api/tidbits/views.py
+++ backend/api/tidbits/views.py
@@ -2,6 +2,7 @@
 import json
 
 from flask import Blueprint, jsonify, request
+from random import randint
 
 from api.rediswrapper import redis
 
@@ -24,6 +25,13 @@ def tidbits_all():
 
 @tidbits_bp.route("/<id>", methods=["GET"])
 def tidbits_id(id):
-	t = redis.lindex("tidbits", id)
-	return jsonify(json.loads(t)), 200
+    t = redis.lindex("tidbits", id)
+    return jsonify(json.loads(t)), 200
 
+@tidbits_bp.route("/random", methods=["GET"])
+def tidbits_random():
+	payload = []
+	index = randint(0,redis.llen("tidbits") - 1)
+	t = json.loads(redis.lindex("tidbits", index))
+	payload.append(t)
+	return jsonify(payload), 200
