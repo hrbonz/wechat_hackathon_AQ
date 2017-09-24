8552962e1936d794c566e78dde7b145c4996df4c
Stefan 'hr' Berder <hr@bonz.org>
2017-09-24 13:20:09 +0800

    api: configuration secrets

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
+import config_secret
+# redis
 REDIS = {
     "host": "localhost",
     "port": 6379,
     "db": 0,
 }
 
+# Flask
 DEBUG = False
 DEBUG_PORT = 8080
+#SERVER_NAME = "mask.measureofquality.com"
+#PREFERRED_URL_SCHEME = "https"
+
+# app
+GAMS_API_KEY = config_secret.GAMS_API_KEY
